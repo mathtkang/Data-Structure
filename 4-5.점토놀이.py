fc8b8e8ec8fec15c3f5d10a9d1fc73c708ccbd31
mathtkang <ksge1124@gmail.com>
2021-07-12 00:11:43 +0900

    n개의 정수를 점토의 무게로 보고 각각의 점토를 한 덩이로 합치는 경우 힘의 최솟값

---
+++ 4-5.점토놀이.py
@@ -0,0 +1,26 @@
+import heapq
+
+
+def getMinForce(weights):
+    '''
+    n개의 점토를 하나로 합치기 위해 필요한 힘의 합의 최솟값을 반환하는 함수를 작성하세요.
+    '''
+    pq = []
+
+    # heapq.heappush()
+
+    for w in weights:
+        heapq.heappush(pq, w)
+
+    result = 0
+
+    while len(pq) > 1:
+        x = heapq.heappop(pq)
+        y = heapq.heappop(pq)
+
+        temp = x + y
+        result += temp
+
+        heapq.heappush(pq, temp)
+
+    return result
